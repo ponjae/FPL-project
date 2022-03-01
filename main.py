dcbe1fbe40b4a6a51d19b3fcb2b15096f949ab2e
ponjae <ponjae11@gmail.com>
2022-03-01 16:46:56 +0100

    Added 404 page

--- main.py
+++ main.py
@@ -11,5 +11,11 @@ def home():
     return render_template("index.html", secret=secret["font_awesome"])
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run()
