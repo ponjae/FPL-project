19fe8da69f27da54c63f73bc6571172e8be4c16c
ponjae <ponjae11@gmail.com>
2022-03-01 16:13:19 +0100

    Added a footer which I am pleased with for the moment

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import imp
 from flask import Flask, render_template
+# from fplData import playerData, playerEvaluation, playerRanking
+from secrets import secret
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", secret=secret["font_awesome"])
 
 
 if __name__ == '__main__':
